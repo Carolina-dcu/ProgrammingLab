832e8fc021ad827e4a64aa5c3a025738cec6a775
repl.it user <replituser@example.com>
2020-11-13 17:16:58 +0000

    aggiunto fie somma.py

---
+++ somma.py
@@ -0,0 +1,11 @@
+x = [2,1,8,5,7,4] # elementi di una lista
+
+somma = 0
+
+for i in x: # uso ciclo for
+    somma+=i
+
+# stampo i valori della lista
+print("Valori della lista:", x) 
+# stampo il risultato
+print("La loro somma è:", somma)
